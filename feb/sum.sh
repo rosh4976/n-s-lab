a1e2eb2810325da51c9c62761bc0f1b5fe8f3bd4
rosh4976 <roshnashirinm2002@gmail.com>
2025-02-21 01:37:09 -0500

    Add files via upload

--- feb/sum.sh
+++ feb/sum.sh
@@ -1,3 +1,9 @@
-#!/bin/bash
-((sum=25+35 ))
-echo "$sum"
+echo "enter the number"
+read n
+sum=0
+while [ $n -gt 0 ]
+do
+	sum=$((sum + n))
+	n=$((n-1))
+done
+echo " the sum of the first n number is :$sum"

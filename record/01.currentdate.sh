67e652d879f093c27c6017f0489cef0af31f638d
rosh4976 <roshnashirinm2002@gmail.com>
2025-06-07 14:37:50 +0530

    Create 01.currentdate.sh

---
+++ record/01.currentdate.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+current_date_time=$(date)
+username=$(whoami)
+current_dir=$(pwd)
+echo "current date nd time :$current_date_time"
+echo "username:$username"
+echo "current working directory:$current_dir"
+
+
+
+
+ouput
+rmurs@Murshi MINGW64 ~/roshna
+$ ./currentdate.sh
+current date nd time :Sat Jun  7 14:34:26 IST 2025
+username:rmurs
+current working directory:/c/Users/rmurs/roshna
+
+
